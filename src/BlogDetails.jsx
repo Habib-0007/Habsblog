4515a88364955baedd7b123f1402634bc44b016e
Habib <109099706+Habib-0007@users.noreply.github.com>
2023-08-28 23:09:14 +0100

    Update BlogDetails.jsx

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -9,7 +9,7 @@ const BlogDetails = () => {
   const history = useHistory();
   
   let handleDelete = () => {
-    fetch("https://api.npoint.io/d9634f01c1e54bc298d2/blogs" + blog.id, { method: "DELETE" })
+    fetch("../data/db.json/blogs" + blog.id, { method: "DELETE" })
     .then( () => {
       alert("Blog successfully deleted")
       history.push("/")
