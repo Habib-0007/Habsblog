b071adb1250998a5ac090487a3f89caa40ee71a1
Habib-0007 <adebayohabib7@gmail.com>
2025-02-03 00:31:29 +0100

    started the styling of the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import type React from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
@@ -10,26 +10,27 @@ import UserProfile from './pages/UserProfile';
 import UserTimeline from './pages/UserTimeline';
 import BlogTimeline from './pages/BlogTimeline';
 import CreatePost from './pages/CreatePost';
+import "./App.css"
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route index path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/user-timeline" element={<UserTimeline />} />
-          <Route path="/blog-timeline" element={<BlogTimeline />} />
-          <Route path="/create-post" element={<CreatePost />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+				<Header />
+			<section className="mt-16">
+				<Routes>
+					<Route index path="/" element={<LandingPage />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+					<Route path="/reset-password/:token" element={<ResetPassword />} />
+					<Route path="/profile" element={<UserProfile />} />
+					<Route path="/user-timeline" element={<UserTimeline />} />
+					<Route path="/blog-timeline" element={<BlogTimeline />} />
+					<Route path="/create-post" element={<CreatePost />} />
+				</Routes>
+			</section>
+		</Router>
+	);
 };
 
 export default App;
